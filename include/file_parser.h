2890119354206b3fe7356c6c65e6774e2535b008
Bartosz <strzelecki100@gmail.com>
2025-12-11 00:14:21 +0100

    Amend file parser signature

--- include/file_parser.h
+++ include/file_parser.h
@@ -2,7 +2,7 @@
 #define AOC_FILE_PARSER_
 
 /* ~~~~~~~~~~~~~~~~ File parser ~~~~~~~~~~~~~~~ */
-typedef void(*AoC_parserFun)(char* line, int len, void* args);
+typedef void(*AoC_parserFun)(char* line, size_t len, void* args);
 int AoC_parseFile(char* file_name, AoC_parserFun parserFunction, void* args);
 
 #endif // AOC_FILE_PARSER_
